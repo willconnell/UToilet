c0e2dc82753ea02af4ce70bb476a0589ae546ca1
Max <43524069+maxtruty@users.noreply.github.com>
2021-10-25 16:30:39 -0500

    Add files via upload

---
+++ UToilet-main/UToilet/AudioViewController.swift
@@ -0,0 +1,62 @@
+//
+//  AudioViewController.swift
+//  UToilet
+//
+//  Created by Max Truty on 10/25/21.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var audioSlider: UISlider!
+    
+    var player: AVAudioPlayer?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playButtonPushed(_ sender: Any) {
+        if let player = player, player.isPlaying {
+            // stop playback
+            
+            player.stop()
+        } else {
+            // set up player, and play
+            let urlString = Bundle.main.path(forResource: "audio", ofType: "mp3")
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+            } catch {
+                print("something went wrong")
+            }
+        }
+    }
+    
+    @IBAction func changeAudio(_ sender: Any) {
+        player?.stop()
+        player?.currentTime = TimeInterval(audioSlider.value)
+        player?.prepareToPlay()
+        player?.play()
+    }
+    
+    
+}
+
---
+++ UToilet-main/UToilet/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UToilet
+//
+//  Created by William Connell on 10/25/21.
+//
+
+// edit to the file
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
