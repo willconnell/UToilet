e9558fd7357eb24f8cce4cfabcfe464c77448e91
William Connell <williamconnell@wireless-10-145-108-36.public.utexas.edu>
2021-10-25 15:51:09 -0500

    first commit

---
+++ UToilet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UToilet
+//
+//  Created by William Connell on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
